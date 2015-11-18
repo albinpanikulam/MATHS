06e5e08b201077609056af6e1b6ed0e89747a8b4
albin <albinpanikulam@gmail.com>
2015-11-18 14:53:11 +0100

    Matematicas.java contiene el metodo: public static boolean esPar

---
+++ Matematicas.java
@@ -0,0 +1,29 @@
+/**clase que va a contener
+ * metodos estaticos de calculo matematico
+ * @author Albin
+ * @version 0.0
+ * */
+public class Matematicas{
+	public static boolean esPar(int numero){
+		/**
+		 * @param numero entero
+		 * @return boolean
+		 * */
+		
+		int resto= numero%2;
+		boolean valor= resto==0;
+		
+		if (valor==true){
+		
+			return true;
+			}
+			else{
+				return false;
+			}
+		}
+	}	
+				
+		
+		
+		
+	
---
+++ TestMatematicas.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class TestMatematicas{
+	public static void main(String[] arg){
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Escribe un numero");
+		int i=sc.nextInt();
+		System.out.println("El numero: "+i+" es "+Matematicas.esPar(i));
+		// System.out.printf("%s %d %s %b %n", );
+		
+		}
+	}
+	
