4282a74b2342fcf25136e8315636b7fe2cdae746
albin <albinpanikulam@gmail.com>
2015-11-18 15:01:49 +0100

    modificado: Matematicas.java 	modificado: TestMatematicas.java

--- Matematicas.java
+++ Matematicas.java
@@ -19,9 +19,49 @@ public class Matematicas{
 			}
 			else{
 				return false;
+
 			}
 		}
-	}	
+			
+			
+		public static boolean esDivisiblePorTres(int numero){	
+			/**
+		 * @param numero entero
+		 * @return boolean
+		 * */
+		
+		int resto= numero%3;
+		boolean valor= resto==0;
+		
+		if (valor==true){
+		
+			return true;
+			}
+			else{
+				return false;
+			}
+		}
+		
+		
+		public static boolean esDivisiblePorCinco(int numero){	
+			/**
+		 * @param numero entero
+		 * @return boolean
+		 * */
+		
+		int resto= numero%5;
+		boolean valor= resto==0;
+		
+		if (valor==true){
+		
+			return true;
+			}
+			else{
+				return false;
+			}
+		}
+	}
+	
 				
 		
 		
--- TestMatematicas.java
+++ TestMatematicas.java
@@ -5,6 +5,8 @@ public class TestMatematicas{
 		System.out.println("Escribe un numero");
 		int i=sc.nextInt();
 		System.out.println("El numero: "+i+" es "+Matematicas.esPar(i));
+		System.out.println("El numero: "+i+" es "+Matematicas.esDivisiblePorTres(i));
+		System.out.println("El numero: "+i+" es "+Matematicas.esDivisiblePorCinco(i));
 		// System.out.printf("%s %d %s %b %n", );
 		
 		}
